db1c007b1823f121e28ee827f755a81ac6f4bc6d
郭海祥 <ghx@strongsoft.net>
2019-11-19 20:35:48 +0800

    Fix
    隐藏左侧返回按钮问题修复

--- HXBaseViewController.podspec
+++ HXBaseViewController.podspec
@@ -20,6 +20,8 @@ Pod::Spec.new do |s|
   s.summary      = "快速搭建界面的导航栏配置"
 
 
+
+
   # This description is used to generate tags and improve search results.
   #   * Think: What does it do? Why did you write it? What is the focus?
   #   * Try to keep it short, snappy and to the point.
--- HXBaseViewController/HXBaseViewController.m
+++ HXBaseViewController/HXBaseViewController.m
@@ -25,7 +25,7 @@
 @interface HXBaseViewController () {
     UINavigationBar * navBar;
     BOOL isVcBaseApper; ///< 导航的设置优先级
-    
+
     UIButton * leftBtn;
 }
 
@@ -44,7 +44,7 @@
 
 + (void)load {
 
-//导航栏按钮位置偏移的解决方案,兼容iOS7~iOS13,可自定义间距
+    //导航栏按钮位置偏移的解决方案,兼容iOS7~iOS13,可自定义间距
 #if __has_include(<UINavigation-SXFixSpace/UINavigationSXFixSpace.h>)
     [UINavigationConfig shared].sx_disableFixSpace = NO;//是否禁止使用修正,默认为NO
     [UINavigationConfig shared].sx_defaultFixSpace = 10;//默认为0 可以修改 ios11+ 距离边缘的间距
@@ -61,44 +61,44 @@
     // Do any additional setup after loading the view.
     self.view.backgroundColor = GBackGroundColor;
 
-    
-    
+
+
     navBar = self.navigationController.navigationBar;
-    
+
     //#pragma mark- 判断 导航的设置优先级 为 控制器 否则有些设置会无效
-    
-     NSNumber * isVCBasedStatusBarAppearance = [[NSBundle mainBundle]objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"];
-    
-//    BOOL isVCBasedStatusBarAppearance = [[[NSBundle mainBundle]objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"] boolValue];
+
+    NSNumber * isVCBasedStatusBarAppearance = [[NSBundle mainBundle]objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"];
+
+    //    BOOL isVCBasedStatusBarAppearance = [[[NSBundle mainBundle]objectForInfoDictionaryKey:@"UIViewControllerBasedStatusBarAppearance"] boolValue];
     //    NSAssert(isVCBasedStatusBarAppearance, @"*******BaseNavViewController已VC作为根视图 \n请在Info.plist中设置UIViewControllerBasedStatusBarAppearance 为YES---\n否则以AppDelegate中的设置为准，或通过[UIApplication sharedApplication] setStatusBarHidden*******");
     if (isVCBasedStatusBarAppearance != nil) {
         isVcBaseApper = ([isVCBasedStatusBarAppearance boolValue])?YES:NO;
     } else {
         isVcBaseApper = YES;
     }
-    
+
     [self setOther];
-    
+
     [self setNavInit];
-    
-    
+
+
 
 }
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    
+
     if (_canRotate) {
         UIInterfaceOrientation interfaceOrientation = (UIInterfaceOrientation)[UIDevice currentDevice].orientation;
         if (interfaceOrientation != UIInterfaceOrientationPortrait && interfaceOrientation != UIInterfaceOrientationUnknown) {
-            
+
             [self hiddenStatusBarOperate:_hiddenStatusBarWhenRotate];
-            
+
         } else {
-            
+
             [self hiddenStatusBarOperate:_hiddenStatusBar];
-            
-            
+
+
         }
     }
 
@@ -118,6 +118,11 @@
         } else {
             [self.navigationController setNavigationBarHidden:_hiddenNavBar animated:NO];
         }
+    }
+
+    if (_hiddenLeftBtn) {
+        self.navigationItem.leftBarButtonItem = nil;
+        self.navigationItem.leftBarButtonItems = nil;
     }
 
 
@@ -158,7 +163,7 @@
 
 #pragma mark- Init Setting
 - (void)setOther {
-    
+
     self.canFullScreenGesBack = YES;
     self.canGesBack = YES;
     self.canRotate = NO;
@@ -166,8 +171,6 @@
     self.hiddenStatusBarWhenRotate = YES;
     self.hiddenNavBar = NO;
     self.edgesForExtendedLayout = UIRectEdgeNone;
-
-
 
 }
 
@@ -179,7 +182,7 @@
      *  请注意＊使用appearance设置UI效果最好采用全局的设置，在所有界面初始化前开始设置，否则可能失效。
      */
     self.statusBarBackgroundColor = GNavBgColor;
-    
+
     //导航栏 字体 字色 背景色
     [navBar
      setTitleTextAttributes:
@@ -189,38 +192,38 @@
        }];
     //导航背景颜色
     self.navBarColor = GNavBgColor;
-    
+
     self.navBarTranslucent = YES;
-    
+
     //状态栏设置
     self.hiddenStatusBar = NO;
-    
+
     //状态栏 字体颜色 黑色/白色 默认白色
     self.statusBarTextIsWhite = YES;
     //导航透明 YES 透明
     self.navBarTranslucent = NO;
     //导航按钮初始化
     [self navBtnInit];
-    
+
 }
 
 
 - (void)navBtnInit {
-    
+
     //导航左按钮
     leftBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     [leftBtn setImageEdgeInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
-    
+
     [leftBtn setImage:[NSString getImageOfImageName:GNavBackImageName type:BaseNavController] forState:UIControlStateNormal];
     [leftBtn addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside];
-    
+
     [leftBtn sizeToFit];
-    
+
     self.cusLeftBtnW = leftBtn.bounds.size.width;
-    
+
     UIBarButtonItem * leftButton = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
     self.navigationItem.leftBarButtonItem = leftButton;
-    
+
 }
 
 #pragma mark- 刷新当前页面 导航控制器 配置
@@ -235,7 +238,7 @@
 }
 
 - (void)setHiddenStatusBar:(BOOL)hidden {
-    
+
     //    if (!hidden) {
     //        return;
     //    }
@@ -248,15 +251,15 @@
     if (!isVcBaseApper) {
         [[UIApplication sharedApplication] setStatusBarHidden:hiddenStatusBar];
     } else {
-        
+
         //prefersStatusBarHidden
-        
+
         if ([self respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)]) {
-            
+
             [self prefersStatusBarHidden];
-            
+
             [self performSelector:@selector(setNeedsStatusBarAppearanceUpdate)];
-            
+
         }
     }
 }
@@ -269,7 +272,7 @@
     } else {
         return _hiddenStatusBar;
     }
-    
+
 }
 ////设置状态栏 风格 VC 不管理，属于UINavigationController管理【不调用】
 - (UIStatusBarStyle)preferredStatusBarStyle {
@@ -285,8 +288,8 @@
     } else {
         [navBar setBarStyle:(isWhite)?UIBarStyleBlack:UIBarStyleDefault];
     }
-    
-    
+
+
 }
 
 //设置状态栏颜色
@@ -306,7 +309,7 @@
         }
     }
 #else
-         statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
+    statusBar = [[[UIApplication sharedApplication] valueForKey:@"statusBarWindow"] valueForKey:@"statusBar"];
 #endif
 
     NSLog(@"statusBar.backgroundColor--->%@",statusBar.backgroundColor);
@@ -319,9 +322,9 @@
 - (void)setNavBarColor:(UIColor *)navColor {
     _navBarColor = navColor;
     if ([[[UIDevice currentDevice] systemVersion] doubleValue] <7.0) {
-        
+
         [navBar setTintColor:navColor];
-        
+
     }else
     {
         [navBar setBarTintColor:navColor];
@@ -333,23 +336,23 @@
     navBar.translucent = translucent;
     if (translucent) {
         self.edgesForExtendedLayout = UIRectEdgeTop;
-        
+
         //设置导航栏背景图片为一个空的image，这样就透明了
         [navBar setBackgroundImage:[[UIImage alloc] init] forBarMetrics:UIBarMetricsDefault];
-        
+
         //去掉透明后导航栏下边的黑边
         [navBar setShadowImage:[[UIImage alloc] init]];
-//        navBar.layer.masksToBounds = YES;// 去掉横线（没有这一行代码导航栏的最下面还会有一个横线）
-        
+        //        navBar.layer.masksToBounds = YES;// 去掉横线（没有这一行代码导航栏的最下面还会有一个横线）
+
     }
     else {
         self.edgesForExtendedLayout = UIRectEdgeNone;
         //    如果不想让其他页面的导航栏变为透明 需要重置
         [navBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
         [navBar setShadowImage:nil];
-        
+
     }
-    
+
 
 }
 
@@ -359,23 +362,28 @@
     _hiddenLeftBtn = hiddenLeftBtn;
     if (_hiddenLeftBtn) {
         self.navigationItem.leftBarButtonItem = nil;
+        self.navigationItem.leftBarButtonItems = nil;
+    } else {
+        if (_leftBtnV) {
+            UIBarButtonItem * leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:_leftBtnV];
+            self.navigationItem.leftBarButtonItem = leftButtonItem;
+        }
     }
-
 }
 
 //手势 返回 开关
 - (void)setCanGesBack:(BOOL)canGesBack {
     _canGesBack = canGesBack;
-    
+
     if ([self.navigationController respondsToSelector:@selector(interactivePopGestureRecognizer)]) {
         self.navigationController.interactivePopGestureRecognizer.enabled = _canGesBack;
-    }   
+    }
 }
 
 - (void)setCanFullScreenGesBack:(BOOL)canFullScreenGesBack {
     _canFullScreenGesBack = canFullScreenGesBack;
 #if __has_include("UINavigationController+FDFullscreenPopGesture.h")
-self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScreenGesBack;
+    self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScreenGesBack;
 #endif
 
 
@@ -387,7 +395,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 }
 
 - (void)setTitleViewCenter:(BOOL)titleViewCenter {
-    
+
     _titleViewCenter = titleViewCenter;
     if (!_titleV) {
         return;
@@ -402,50 +410,52 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 #pragma mark- 文字标题
 
 - (void)setRightStr:(NSString *)rightStr {
-    
+
     //导航左按钮
     UIButton * rightBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    
+
     [rightBtn setImageEdgeInsets:UIEdgeInsetsMake(10, 0, 5, 15)];
-    
+
     [rightBtn addTarget:self action:@selector(rightBtnClick) forControlEvents:UIControlEventTouchUpInside];
     [rightBtn setTitle:rightStr forState:UIControlStateNormal];
     [rightBtn setTitleColor:GNavBtnTitleColorNormal forState:UIControlStateNormal];
     [rightBtn setTitleColor:GNavBtnTitleColorSelect forState:UIControlStateHighlighted];
     rightBtn.titleLabel.font = GNavBtnTitleFont;
-    
+
     [rightBtn sizeToFit];
-    
+
     self.cusRightBtnW = rightBtn.bounds.size.width;
-    
-    
+
+
     UIBarButtonItem * rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightBtn];
     self.navigationItem.rightBarButtonItem = rightButtonItem;
-    
+
 }
 
 - (void)setBackStr:(NSString *)backStr {
-    
+
     if (self.hiddenLeftBtn) {
+        self.navigationItem.leftBarButtonItem = nil;
+        self.navigationItem.leftBarButtonItems = nil;
         return;
     }
-    
+
     //导航左按钮
     leftBtn = [UIButton buttonWithType:UIButtonTypeCustom];
-    
+
     [leftBtn addTarget:self action:@selector(backBtnClick) forControlEvents:UIControlEventTouchUpInside];
     [leftBtn setTitle:backStr forState:UIControlStateNormal];
     [leftBtn setTitleColor:GNavBtnTitleColorNormal forState:UIControlStateNormal];
     [leftBtn setTitleColor:GNavBtnTitleColorSelect forState:UIControlStateHighlighted];
     leftBtn.titleLabel.font = GNavBtnTitleFont;
-    
+
     [leftBtn sizeToFit];
-    
+
     self.cusLeftBtnW = leftBtn.bounds.size.width;
-    
+
     UIBarButtonItem * leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
     self.navigationItem.leftBarButtonItem = leftButtonItem;
-    
+
 }
 
 - (void)setBackImage:(UIImage *)backImage {
@@ -456,7 +466,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 - (void)setNavBgImage:(UIImage *)bgImage {
     _navBgImage = bgImage;
     if (_navBgImage) {
-         [self.navigationController.navigationBar setBackgroundImage:bgImage forBarMetrics:UIBarMetricsDefault];
+        [self.navigationController.navigationBar setBackgroundImage:bgImage forBarMetrics:UIBarMetricsDefault];
     }
 
 }
@@ -473,14 +483,18 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 #pragma mark- 自定义视图
 - (void)setLeftBtnV:(UIView *)leftBtnV {
     if (!leftBtnV) {
+        self.navigationItem.leftBarButtonItem = nil;
+        self.navigationItem.leftBarButtonItems = nil;
         return;
     }
     if (self.hiddenLeftBtn) {
+        self.navigationItem.leftBarButtonItem = nil;
+        self.navigationItem.leftBarButtonItems = nil;
         return;
     }
     _leftBtnV = leftBtnV;
     self.cusLeftBtnW = _leftBtnV.bounds.size.width;
-    
+
     UIBarButtonItem * leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtnV];
     self.navigationItem.leftBarButtonItem = leftButtonItem;
 }
@@ -491,7 +505,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
     }
     _rightBtnV = rightBtnV;
     self.cusRightBtnW = _rightBtnV.bounds.size.width;
-    
+
     UIBarButtonItem * rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightBtnV];
     self.navigationItem.rightBarButtonItem = rightButtonItem;
 }
@@ -500,37 +514,37 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
     if (imageArr.count == 0 || imageArr.count > 2) {
         return;
     }
-    
+
     if (!clickBlock) {
         return;
     }
     _cusNavClickIndex = clickBlock;
-    
+
     UIView * cusView = [[UIView alloc] initWithFrame:CGRectZero];
-    
+
     cusView.frame = CGRectMake(0, 0, (imageArr.count == 1)?40:80, 40);
-    
+
     for (int i = 0; i < imageArr.count; i ++) {
         UIButton * btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        
+
         btn.tag = 10086 + i;
-        
+
         btn.frame = CGRectMake((i == 0)?0:40, 2, 40, 40);
         [btn setImage:[UIImage imageNamed:imageArr[i]] forState:UIControlStateNormal];
         [btn addTarget:self action:@selector(cusRightBtnClick:) forControlEvents:UIControlEventTouchUpInside];
-        
+
         [btn setImageEdgeInsets:UIEdgeInsetsMake(10, 10, 10, 10)];
-        
+
         //        [btn setTitleColor:GNavBtnTitleColorNormal forState:UIControlStateNormal];
         //        [btn setTitleColor:GNavBtnTitleColorSelect forState:UIControlStateHighlighted];
         //        btn.titleLabel.font = GNavBtnTitleFont;
-        
+
         [cusView addSubview:btn];
-        
+
     }
-    
+
     self.cusRightBtnW = cusView.bounds.size.width;
-    
+
     UIBarButtonItem * rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:cusView];
     self.navigationItem.rightBarButtonItem = rightButtonItem;
 }
@@ -539,19 +553,19 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
     if (!titleV) {
         return;
     }
-    
+
     _titleV = titleV;
-    
+
     if (_titleViewCenter) {
         CGRect frame = [self getMaxTitleViewRect];
         titleV.frame = frame;
     }
-    
+
     NSLog(@"------- %f %f %f %f",titleV.frame.origin.x,titleV.frame.origin.y,titleV.frame.size.width,titleV.frame.size.height);
     if (self.navigationItem.titleView) {
         self.navigationItem.titleView = nil;
     }
-    
+
     self.navigationItem.titleView = titleV;
 }
 
@@ -562,16 +576,16 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
  *  @return rect
  */
 - (CGRect)getMaxTitleViewRect {
-    
+
     CGRect frame = CGRectZero;
-    
+
     CGFloat maxWidth = _cusLeftBtnW > _cusRightBtnW ? _cusLeftBtnW : _cusRightBtnW;
-    
+
     maxWidth += 15 + 15;//leftview 左右都有间隙，左边是5像素，右边是8像素，加2个像素的阀值 5 ＋ 8 ＋ 2
-    
+
     frame.size.width = [UIScreen mainScreen].bounds.size.width - maxWidth * 2;
     frame.size.height = 44;
-    
+
     return frame;
 }
 
@@ -607,13 +621,13 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 - (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration {
     NSLog(@"rotate TO %ld",(long)toInterfaceOrientation);
     if (toInterfaceOrientation != UIInterfaceOrientationPortrait && toInterfaceOrientation != UIInterfaceOrientationUnknown) {
-        
-         [self hiddenStatusBarOperate:_hiddenStatusBarWhenRotate];
-        
+
+        [self hiddenStatusBarOperate:_hiddenStatusBarWhenRotate];
+
     } else {
-        
-         [self hiddenStatusBarOperate:_hiddenStatusBar];
-        
+
+        [self hiddenStatusBarOperate:_hiddenStatusBar];
+
 
     }
 }
@@ -625,7 +639,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 }
 //支持那些旋转方向（project设置的）
 - (UIInterfaceOrientationMask)supportedInterfaceOrientations {
-    
+
     return (_canRotate)?UIInterfaceOrientationMaskAll:UIInterfaceOrientationMaskPortrait;
 }
 
@@ -636,19 +650,19 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 
 #pragma mark- Util
 - (instancetype)setTabBarItemWithTitle:(NSString *)title
-               titleUnSelectStyle:(NSDictionary *)titleUnSelectStyle
-                 titleSelectStyle:(NSDictionary *)titleSelectStyle
-                    unselectImage:(UIImage *)unselectImage
-                      selectImage:(UIImage *)selectImage
-                        imageSize:(CGSize)imageSize {
-    
+                    titleUnSelectStyle:(NSDictionary *)titleUnSelectStyle
+                      titleSelectStyle:(NSDictionary *)titleSelectStyle
+                         unselectImage:(UIImage *)unselectImage
+                           selectImage:(UIImage *)selectImage
+                             imageSize:(CGSize)imageSize {
+
     self.tabBarItem.title = title;
     [self.tabBarItem setSelectedImage:[self.class imageWithSize:imageSize image:selectImage]];
     [self.tabBarItem setImage:[self.class imageWithSize:imageSize image:unselectImage]];
-    
+
     [self.tabBarItem setTitleTextAttributes:titleUnSelectStyle forState:UIControlStateNormal];
     [self.tabBarItem setTitleTextAttributes:titleSelectStyle forState:UIControlStateSelected];
-    
+
     return self;
 }
 
@@ -661,7 +675,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
     UIGraphicsBeginImageContext(size);
     [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
     image =   [UIGraphicsGetImageFromCurrentImageContext() imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
-    
+
     UIGraphicsEndImageContext();
     return image;
 }
@@ -670,7 +684,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 
 - (void)pushRootNav:(UIViewController *)viewController animated:(BOOL)animated {
 
-   id rootVC = [UIApplication sharedApplication].delegate.window.rootViewController;
+    id rootVC = [UIApplication sharedApplication].delegate.window.rootViewController;
     if ([rootVC isKindOfClass:[UINavigationController class]]) {
         UINavigationController * rootVCNav = rootVC;
         [rootVCNav pushViewController:viewController animated:animated];
@@ -680,7 +694,7 @@ self.navigationController.fd_fullscreenPopGestureRecognizer.enabled = canFullScr
 }
 /*
  #pragma mark - Navigation
- 
+
  // In a storyboard-based application, you will often want to do a little preparation before navigation
  - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
  // Get the new view controller using [segue destinationViewController].
